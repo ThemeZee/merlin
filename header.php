04c9de4117a9c8291a5d86a54a89df6bf273e4be
Thomas W <netzberufler@googlemail.com>
2015-02-12 13:32:20 +0100

    added unminified javascript

--- header.php
+++ header.php
@@ -10,7 +10,7 @@
 
 <?php /* Embeds HTML5shiv to support HTML5 elements in older IE versions plus CSS Backgrounds */ ?>
 <!--[if lt IE 9]>
-<script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
+<script src="<?php echo get_template_directory_uri(); ?>/js/html5shiv.min.js" type="text/javascript"></script>
 <![endif]-->
 <?php wp_head(); ?>
 </head>
