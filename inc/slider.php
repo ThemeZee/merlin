1c69553bbd452549339463a0edc11d5c5f3b793d
Thomas W <netzberufler@googlemail.com>
2016-04-12 11:02:37 +0200

    updated flexslider

--- inc/slider.php
+++ inc/slider.php
@@ -26,7 +26,7 @@ function merlin_slider_scripts() {
 		wp_enqueue_style( 'merlin-flexslider', get_template_directory_uri() . '/css/flexslider.css' );
 
 		// FlexSlider JS
-		wp_enqueue_script( 'merlin-flexslider', get_template_directory_uri() .'/js/jquery.flexslider-min.js', array('jquery'), '2.5.0' );
+		wp_enqueue_script( 'merlin-flexslider', get_template_directory_uri() .'/js/jquery.flexslider-min.js', array('jquery'), '2.6.0' );
 
 		// Register and enqueue slider.js
 		wp_enqueue_script( 'merlin-post-slider', get_template_directory_uri() .'/js/slider.js', array('merlin-flexslider') );
